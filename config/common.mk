edd403cb9c36df9eec35a6d5172aabecdc1e8347
Anay Wadhera <awadhera@berkeley.edu>
2020-10-02 16:26:35 -0700

    statix: allow customizing the length of gesture navbar [3/3]
    Change-Id: I9fc95f315d5cf7a2b9a90af9fdce9eadce518653
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/3139
    Tested-by: Andrew Thomas <itz63c@statixos.com>
    Reviewed-by: Andrew Thomas <itz63c@statixos.com>

--- config/common.mk
+++ config/common.mk
@@ -272,4 +272,9 @@ PRODUCT_PACKAGES += \
     QStileTriangles \
     QStileCircleOutline
 
+    # Navbar
+PRODUCT_PACKAGES += \
+    GesturalNavigationOverlayLong \
+    GesturalNavigationOverlayMedium
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

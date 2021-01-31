3547ba4e217ad544fef7eb2dd85cbd6dda10a907
madmax7896 <thegreatsagar123@gmail.com>
2021-01-31 13:33:12 +0530

    vendor: Cleanup

--- config/common.mk
+++ config/common.mk
@@ -123,28 +123,6 @@ PRODUCT_PACKAGES += \
     StitchImage \
     ThemePicker
 
-# Custom Overlays
-# Settings
-PRODUCT_PACKAGES += \
-    SystemPitchBlackOverlay \
-    SystemUIPitchBlackOverlay \
-    SystemDarkGrayOverlay \
-    SystemUIDarkGrayOverlay \
-    SystemStyleOverlay \
-    SystemUIStyleOverlay \
-    SystemNightOverlay \
-    SystemUINightOverlay \
-    SystemSolarizedDarkOverlay \
-    SystemUISolarizedDarkOverlay \
-    SystemMaterialOceanOverlay \
-    SystemUIMaterialOceanOverlay \
-    SystemBakedGreenOverlay \
-    SystemUIBakedGreenOverlay \
-    SystemChocoXOverlay \
-    SystemUIChocoXOverlay \
-    SystemDarkAubergineOverlay \
-    SystemUIDarkAubergineOverlay
-
 # SystemUI plugins
 PRODUCT_PACKAGES += \
     QuickAccessWallet
